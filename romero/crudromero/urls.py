f493036b44f8b901840ccd54adeabe639e1b3a5c
Juan Manuel Ostiz <w4mp1z@outlook.com>
2022-07-14 14:28:10 -0300

    Cambios a la vista de todos los datos (agregado el botón de página anterior, comportamiento y correcciones)

--- romero/crudromero/urls.py
+++ romero/crudromero/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('', views.inicio, name='inicio'),
     path('getuserdata/', views.get_data, name='api-get'),
-    path('ver-todo/<str:text>/<int:fin>/', views.ver_todo, name='ver_todo'),
+    path('ver-todo/<str:text>/<int:fin>/<str:anchor>/', views.ver_todo, name='ver_todo'),
     path('ver/<str:id>', views.vista, name='vista'),
     path('modificar/<int:id>', views.modificar, name='modificar'),
 ]
--- romero/crudromero/views.py
+++ romero/crudromero/views.py
@@ -26,33 +26,38 @@ def vista(request, id):
     context = {'paciente': Paciente.objects.get(id=id)}
     return render(request, 'crudromero/ver.html', context)
 
-def get_query_result(text, all=False, fin=0):
+def get_query_result(text, all=False, fin=0, anchor=None, grow_value=100):
 
-    start_value, limit = (0, 5) if not all else (fin, fin+100)
+    start_value, limit = (0, 5) if not all else (fin, fin + grow_value) if anchor == 'next' else (fin - (grow_value*2), fin - grow_value)
 
     if text[1] == 'undefined':
         if text[0].isdigit():
-            pacientes = Paciente.objects.filter(dni__startswith=text[0])[start_value:limit]
+            pacientes = Paciente.objects.filter(dni__startswith=text[0])[start_value:limit+1]
         else:
-            pacientes = Paciente.objects.filter(nombre__startswith=text[0])[start_value:limit]
+            pacientes = Paciente.objects.filter(nombre__startswith=text[0])[start_value:limit+1]
     else:
         if text[0].isdigit():
-            pacientes = Paciente.objects.filter(dni__startswith=text[0], nombre__startswith=text[1])[start_value:limit]
+            pacientes = Paciente.objects.filter(dni__startswith=text[0], nombre__startswith=text[1])[start_value:limit+1]
         else:
-            pacientes = Paciente.objects.filter(dni__startswith=text[1], nombre__startswith=text[0])[start_value:limit]
+            pacientes = Paciente.objects.filter(dni__startswith=text[1], nombre__startswith=text[0])[start_value:limit+1]
+    
+    
+    more = len(pacientes) == (limit-start_value)+1 and len(pacientes) != 0
 
-    d_none = True if len(pacientes) == 0 else False
+    return pacientes[:limit-start_value], limit, more
 
-    return pacientes, limit, d_none
+def ver_todo(request, text, fin, anchor):
 
-def ver_todo(request, text, fin):
+    grow_value = 100
 
-    pacientes, fin, d_none = get_query_result(text.split('-'), True, fin)
+    pacientes, fin, more = get_query_result(text.split('-'), True, fin, anchor, grow_value)
+    print(more)
     context = {
             'text': text,
             'fin': fin,
             'pacientes': pacientes,
-            'd_none': ('d-none' if d_none else ''),
+            'd_none_back': ('d-none' if fin == grow_value else ''),
+            'd_none_next': ('d-none' if not more else ''),
     }
 
     return render(request, 'crudromero/ver_todo.html', context)
