edb3920e54f04c14f0b2db2b4e020209c5844d14
Isidro Emir Pelli <isidroemirpelli@gmail.com>
2022-07-24 18:52:32 -0300

    Integracion de Tkinter

---
+++ romero/main.py
@@ -0,0 +1,27 @@
+from subprocess import Popen
+from sys import argv
+from os import kill
+from signal import CTRL_BREAK_EVENT
+from PyQt5.Qt import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtWidgets import QApplication
+
+
+def start_django():
+    django_server = Popen(['python', 'manage.py', 'runserver'])
+    return django_server.pid
+
+
+def generate_browser():
+    app = QApplication(argv)
+    app.setApplicationName("Base de datos - Romero")
+    web = QWebEngineView()
+    web.load(QUrl("http://127.0.0.1:8000"))
+    web.showMaximized()
+    app.exec_()  # This is the main loop of the application.
+
+
+if __name__ == '__main__':
+    pid = start_django()  # Start django server and get the process id
+    generate_browser()
+    kill(pid, CTRL_BREAK_EVENT)
