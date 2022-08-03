f9c7abb84ae5aa9629b66abade5fed5bdb19c517
Juan Manuel Ostiz <w4mp1z@outlook.com>
2022-08-02 23:31:19 -0300

    Ahora se espera a que el server inicie

--- romero/main.py
+++ romero/main.py
@@ -1,4 +1,4 @@
-from subprocess import Popen, call
+from subprocess import Popen, call, PIPE
 from sys import argv
 from os import kill
 from os.path import exists
@@ -11,7 +11,9 @@ from PyQt5.QtGui import QIcon
 
 def start_django():
     django_server = Popen(
-        ['python', 'manage.py', 'runserver', '--insecure'], shell=True)
+        ['python', 'manage.py', 'runserver', '--insecure'], stdout=PIPE, shell=True
+    )
+
     return django_server
 
 
@@ -24,6 +26,11 @@ def generate_browser():
     web.showMaximized()
     app.exec_()  # This is the main loop of the application.
 
+def await_server(django_server):
+    while True:
+        output = django_server.stdout.readline()
+        if str(output).__contains__("CTRL-BREAK"):
+            break
 
 if __name__ == '__main__':
     # Si no existe la base de datos, hago la migración.
@@ -31,6 +38,8 @@ if __name__ == '__main__':
         call(['python', 'manage.py', 'migrate'])
 
     django_server = start_django()  # Start django server and get the process id
+    
+    await_server(django_server)    
     generate_browser()
     try:
         kill(django_server.pid, CTRL_BREAK_EVENT)
